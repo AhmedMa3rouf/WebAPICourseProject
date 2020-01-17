d5a0590f1d43baa494b31614d6f6ac08820f3b86
=Ahmed Ma3rouf <Ahmed.Ma3rouf@hotmail.com>
2020-01-17 22:07:30 +0200

    A (BusinessService Project, IProductService, ProductController)

---
+++ BusinessServices/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BusinessEntities;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessServices
+{
+    public static class AutoMapperConfig
+    {
+        public static void CreateMapping()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Product, ProductEntity>();
+            });
+        }
+    }
+}
